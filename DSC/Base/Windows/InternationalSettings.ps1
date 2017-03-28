728b180100638d2821601f9a088fd4a1edc9e1f7
Paul Marston <paul@marston.me>
2017-03-28 02:04:56 +0100

    h

--- DSC/Base/Windows/InternationalSettings.ps1
+++ DSC/Base/Windows/InternationalSettings.ps1
@@ -7,16 +7,13 @@ Script InternationalSettings
         Set-Culture -CultureInfo $enGb
         Set-WinHomeLocation -GeoId 242
         Set-WinSystemLocale -SystemLocale $enGb
-        Set-WinUserLanguageList -LanguageList ([Microsoft.InternationalSettings.Commands.WinUserLanguage]::new('en-GB')) -Force
-        $global:DSCMachineStatus = 1
     }
     TestScript = {
         Import-Module International
 
         $null -ne (Get-Culture | ? Name -eq 'en-GB') -and `
         $null -ne (Get-WinHomeLocation | ? HomeLocation -eq 'United Kingdom') -and `
-        $null -ne (Get-WinSystemLocale | ? Name -eq 'en-GB') -and `
-        $null -ne (Get-WinUserLanguageList | % GetEnumerator | ? LanguageTag -eq 'en-GB')
+        $null -ne (Get-WinSystemLocale | ? Name -eq 'en-GB')
     }
     GetScript = { @{} }
 }
