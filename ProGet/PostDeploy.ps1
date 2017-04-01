a219b097331f2c75df9d0bd91f54bd8871c8efd1
Paul Marston <paul@marston.me>
2017-04-01 11:40:24 +0100

    j

--- ProGet/PostDeploy.ps1
+++ ProGet/PostDeploy.ps1
@@ -1,3 +1,3 @@
 
-& C:\ProGet\DbChangeScripter\bmdbupdate.exe UPDATE /init=false /conn=#{ConnectionString}
+& C:\ProGet\DbChangeScripter\bmdbupdate.exe UPDATE /init=false /conn="#{ConnectionString}"
 & C:\ProGet\Service\ProGet.Service.exe install
