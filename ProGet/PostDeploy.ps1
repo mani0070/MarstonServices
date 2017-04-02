e670174579c7fd2817835f550da62bebf11f3749
Paul Marston <paul@marston.me>
2017-04-02 03:23:49 +0100

    fg

--- ProGet/PostDeploy.ps1
+++ ProGet/PostDeploy.ps1
@@ -1,3 +1,6 @@
+if (Get-Service INEDOPROGETSVC -ErrorAction Ignore) {
+    Stop-Service INEDOPROGETSVC -Force -Verbose
+}
 
 & C:\ProGet\DbChangeScripter\bmdbupdate.exe UPDATE /init=false /conn="${ConnectionString}"
 if ($LASTEXITCODE -ne 0) { throw "bmdbupdate.exe $LASTEXITCODE" }
