98ef37a10911c31a237d5377a332664616988128
Paul Marston <paul@marston.me>
2017-04-02 02:32:04 +0100

    s

--- ProGet/PostDeploy.ps1
+++ ProGet/PostDeploy.ps1
@@ -1,3 +1,6 @@
-
+Write-Host "#{ConnectionString}"
 & C:\ProGet\DbChangeScripter\bmdbupdate.exe UPDATE /init=false /conn="#{ConnectionString}"
+if ($LASTEXITCODE -ne 0) { throw "bmdbupdate.exe $LASTEXITCODE" }
 & C:\ProGet\Service\ProGet.Service.exe install
+if ($LASTEXITCODE -ne 0) { throw "bmdbupdate.exe $LASTEXITCODE" }
+Start-Service INEDOPROGETSVC -Verbose
