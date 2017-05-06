bee256bf3b8b32f30d94cc1ad1f979c06c81e55c
Paul Marston <paul@marston.me>
2017-05-06 21:19:56 +0100

    t

--- DSC/Base/Windows/InternationalSettings.ps1
+++ DSC/Base/Windows/InternationalSettings.ps1
@@ -7,13 +7,15 @@ Script InternationalSettings
         Set-Culture -CultureInfo $enGb
         Set-WinHomeLocation -GeoId 242
         Set-WinSystemLocale -SystemLocale $enGb
+        Set-TimeZone -Id 'GMT Standard Time'
     }
     TestScript = {
         Import-Module International
 
         $null -ne (Get-Culture | ? Name -eq 'en-GB') -and `
         $null -ne (Get-WinHomeLocation | ? HomeLocation -eq 'United Kingdom') -and `
-        $null -ne (Get-WinSystemLocale | ? Name -eq 'en-GB')
+        $null -ne (Get-WinSystemLocale | ? Name -eq 'en-GB') -and `
+        $null -ne (Get-TimeZone | ? Id -eq 'GMT Standard Time')
     }
     GetScript = { @{} }
 }
